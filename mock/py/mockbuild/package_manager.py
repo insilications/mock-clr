344f655b1731e815f6f4caf606c96b05b330ee02
Francisco Boni <boboniboni@gmail.com>
2021-08-15 03:22:27 -0300

    Copy Clear Linux certificates

--- mock/py/mockbuild/package_manager.py
+++ mock/py/mockbuild/package_manager.py
@@ -303,19 +303,18 @@ Error:      Neither dnf-utils nor yum-utils are installed. Dnf-utils or yum-util
 
     @traceLog()
     def copy_certs(self):
-        cert_paths = ["/etc/pki/ca-trust", "/usr/share/pki/ca-trust-source"]
-        for cert_path in cert_paths:
-            pki_dir = self.buildroot.make_chroot_path(cert_path)
-            try:
-                copy_tree(cert_path, pki_dir)
-            except DistutilsFileError:
-                warning = "Couldn't copy certs from host, %s doesn't exist or is not readable"
-                self.buildroot.root_log.debug(warning, cert_path)
+        cert_path = "/var/cache/ca-certs"
+        pki_dir = self.buildroot.make_chroot_path(cert_path)
+        try:
+            copy_tree(cert_path, pki_dir)
+        except DistutilsFileError:
+            warning = "Couldn't copy certs from host, %s doesn't exist or is not readable"
+            self.buildroot.root_log.debug(warning, cert_path)
 
         bundle_path = self.config['ssl_ca_bundle_path']
         if bundle_path:
             self.buildroot.root_log.debug('copying CA bundle into chroot')
-            host_bundle = os.path.realpath('/etc/pki/tls/certs/ca-bundle.crt')
+            host_bundle = os.path.realpath('/var/cache/ca-certs/compat/ca-roots.pem')
             chroot_bundle_path = self.buildroot.make_chroot_path(bundle_path)
             chroot_bundle_dir = os.path.dirname(chroot_bundle_path)
             try:
